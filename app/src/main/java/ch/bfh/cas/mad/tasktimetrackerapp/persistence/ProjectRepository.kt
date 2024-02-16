6dfff111c9e63b7685bd7b8b67fbc338af4f8561
Marc Schärer <scham36@bfh.ch>
2024-02-16 20:54:27 +0100

    remove helper data

--- app/src/main/java/ch/bfh/cas/mad/tasktimetrackerapp/persistence/ProjectRepository.kt
+++ app/src/main/java/ch/bfh/cas/mad/tasktimetrackerapp/persistence/ProjectRepository.kt
@@ -22,14 +22,7 @@ class ProjectRepository(
             return@withContext storedEntries
         }
 
-        //return@withContext emptyList<Entry>().toMutableList()
-        // ToDo: remove this initial list
-        if (projectId == 1)
-        {
-            return@withContext mutableListOf(Entry(1,"Test entry 1 P1", 1, 100), Entry(2,"Test entry 2 P1", 1, 200), Entry(3,"Test entry 3 P1", 4, 300))
-        }
-        else
-            return@withContext mutableListOf(Entry(1,"Test entry 1 P?", 1, 100), Entry(2,"Test entry 2 P?", 1, 200), Entry(3,"Test entry 3 P?", 4, 300))
+        return@withContext emptyList<Entry>().toMutableList()
     }
 
     suspend fun deleteAllProjects() {
