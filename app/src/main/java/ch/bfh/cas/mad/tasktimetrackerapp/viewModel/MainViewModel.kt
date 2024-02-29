70b06e5b0809681afccce99eee7ead8f2be14aa8
Marc Schärer <scham36@bfh.ch>
2024-02-29 12:38:03 +0100

    getTaskName in MainViewModel

--- app/src/main/java/ch/bfh/cas/mad/tasktimetrackerapp/viewModel/MainViewModel.kt
+++ app/src/main/java/ch/bfh/cas/mad/tasktimetrackerapp/viewModel/MainViewModel.kt
@@ -13,9 +13,19 @@ class MainViewModel (
     private var _widgetTasks = MutableStateFlow(emptyList<WidgetTask>().toMutableList())
     val widgetTasks: MutableStateFlow<MutableList<WidgetTask>> = _widgetTasks
 
+    private var _taskName = MutableStateFlow("")
+    val taskName: MutableStateFlow<String> = _taskName
+
     fun getAllWidgetTasks() {
         viewModelScope.launch {
             _widgetTasks.value = widgetTaskRepository.getAllWidgetTasks()
+        }
+    }
+
+    fun getTaskName(widgetTaskId: Int) {
+        viewModelScope.launch {
+            val task = widgetTaskRepository.getTaskForId(widgetTaskId)
+            _taskName.value = task.name
         }
     }
 }
