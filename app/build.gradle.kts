cc1dee9ff70d6da471e7c9f042162351ed90b7e6
Marc Schärer <scham36@bfh.ch>
2024-03-05 14:57:30 +0100

    enable SDK33 for real device

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
 
     defaultConfig {
         applicationId = "ch.bfh.cas.mad.tasktimetrackerapp"
-        minSdk = 34
-        targetSdk = 34
+        minSdk = 33
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
